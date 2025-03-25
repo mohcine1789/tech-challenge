31b191ede7b9db70a6dcc7714c7fbcded929d9fd
mohcine1789 <moxchine1789@gmail.com>
2025-03-25 17:37:11 +0100

    add rety mechanism

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 2,
+  },
+
   defaultCommandTimeout: 6000,
 
   e2e: {
